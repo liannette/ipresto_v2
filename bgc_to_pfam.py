82f0ae49ab867a0a3b459b8290884c1abce56077
Joris Louwen <joris.louwen@wur.nl>
2019-03-25 17:34:21 +0100

    Finished making fastafiles

--- bgc_to_pfam.py
+++ bgc_to_pfam.py
@@ -13,7 +13,7 @@ Fasta files created in input_folder_fasta
 Only handles gbk files with one cluster
 
 TODO:
-
+    write excluded files to a file and the reason of exclusion.
 '''
 
 from sys import argv
@@ -30,6 +30,8 @@ def process_gbks(input_folder, exclude, exclude_contig_edge,
     input_folder: str
     exclude: list of str, files will be excluded if part of the file name
         is present in this list
+    exclude_contig_edge: bool
+    min_genes: int
     '''
     if input_folder.endswith('/'):
         out_folder_base, inf = os.path.split(input_folder[:-1])
@@ -50,7 +52,8 @@ def process_gbks(input_folder, exclude, exclude_contig_edge,
         else:
             convert_gbk2fasta(file_path, out_fasta, exclude_contig_edge,
                 min_genes)
-            exit()
+            #if i > 5:
+                #exit()
         processed +=1
     print("Processed {} files and excluded {} files containing {}".format(\
         processed, excluded, ' or '.join(exclude)))
@@ -59,28 +62,60 @@ def convert_gbk2fasta(file_path, out_folder, exclude_contig_edge, min_genes):
     '''Convert one gbk file to a fasta file in out_folder
 
     file_path, out_folder: strings
+    exclude_contig_edge: bool
+    min_genes: int
     '''
     file_name = os.path.split(file_path)[1]
+    name = file_name.strip('.gbk')
+    outfile = os.path.join(out_folder, '{}.fasta'.format(name))
     seqs = OrderedDict()
-    try:
-        record = next(SeqIO.parse(file_path, 'genbank'))
-    except ValueError as e:
-        print(" {} in file {}. File will be excluded.".format(e, file_path))
-        return
-    for feature in record.features:
-        if feature.type == 'cluster':
-            if "contig_edge" in feature.qualifiers:
-                if feature.qualifiers["contig_edge"][0] == "True":
-                    if exclude_contig_edge:
-                        print(" Contig edge detected in {}".format(file_name))
-                        return
+    num_genes = 0
+    if not os.path.exists(outfile):
+        try:
+            record = next(SeqIO.parse(file_path, 'genbank'))
+        except ValueError as e:
+            print(" Excluding {}: {}".format(file_path, e))
+            return
+        for feature in record.features:
+            if feature.type == 'cluster':
+                if "contig_edge" in feature.qualifiers:
+                    if feature.qualifiers["contig_edge"][0] == "True":
+                        if exclude_contig_edge:
+                            print(" Excluding {}: {}".format(file_name,\
+                                "contig edge"))
+                            return
+            if feature.type == 'CDS':
+                #print(feature.qualifiers)
+                header = ">{}_{}".format(name, num_genes+1)
+                seqs[header] = feature.qualifiers['translation'][0]
+                num_genes +=1
 
-        if feature.type == 'CDS':
-            print(feature.qualifiers)
+        if num_genes < min_genes:
+            print(" Excluding {}: less than {} genes".format(file_path,\
+                min_genes))
+            return
+        with open(outfile, 'w') as out:
+            for seq in seqs:
+                out.write('{}\n{}\n'.format(seq, seqs[seq]))
 
+def runHmmScan(fastaPath, hmmPath, outputdir, verbose):
+    """ Runs hmmscan command on a fasta file with a single core to generate a
+    domtable file"""
+    hmmFile = os.path.join(hmmPath,"Pfam-A.hmm")
+    if os.path.isfile(fastaPath):
+        name = ".".join(fastaPath.split(os.sep)[-1].split(".")[:-1])
+        outputName = os.path.join(outputdir, name+".domtable")
+        
+        hmmscan_cmd = "hmmscan --cpu 0 --domtblout {} --cut_tc {} {}".format(outputName, hmmFile, fastaPath)
+        if verbose == True:
+            print("   " + hmmscan_cmd)
+        subprocess.check_output(hmmscan_cmd, shell=True)
+    else:
+        raise SystemExit("Error running hmmscan: {} doesn't exist".format(\
+            fastaPath))
 
 if __name__ == "__main__":
     in_folder = argv[1]
 
-    process_gbks(in_folder, exclude = ['final'], exclude_contig_edge = True,
-        min_genes = 5)
+    fasta_folder = process_gbks(in_folder, exclude = ['final'],
+        exclude_contig_edge = True, min_genes = 5)
