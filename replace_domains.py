8ec1bbcde0069d975ee333ea4e5f6179dad43086
Joris Louwen <joris.louwen@wur.nl>
2019-03-27 16:13:07 +0100

    read txt file

--- replace_domains.py
+++ replace_domains.py
@@ -3,11 +3,28 @@
 Author: Joris Louwen
 Student number: 960516530090
 
-Script to convert hmm domain models by subdomain models in a hmm flatfile
+Script to delete certain hmm models from a hmm flatfile based on
+domain names provided in a text file
 
 Usage:
-    python3 replace_domains.py <main_hmm> <replace_hmms>
+    python3 replace_domains.py <main_hmm> <delete_domains.txt>
 
 Notes:
-File name of replace_hmm should reflect the Pfam to be replaced
+domains in delete_domains should be on seperate lines
 '''
+
+from sys import argv
+
+def read_dels(filename):
+    '''Reads text file into list
+    '''
+    with open(filename, 'r') as infile:
+        lines = [line.strip() for line in infile]
+    return lines
+
+if __name__ == "__main__":
+    hmm_file = argv[1]
+    del_file = argv[2]
+
+    del_list = read_dels(del_file)
+    print(del_list)
