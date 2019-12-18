1e495d8a28e9bdd6e37cea4cb7fa7f66ddb34d51
Joris Louwen <joris.louwen@wur.nl>
2019-12-18 15:23:03 +0100

    allow all scripts to be added to path, so they can be run from cmd

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import setuptools
+import glob
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+#add all scripts to path, to be able to run from commandline
+all_scripts = glob.glob('**/*[A-Za-z0-9].py', recursive=True)
+
 setuptools.setup(
     name="iPRESTO",
-    version="1.0.1",
+    version="1.0.2",
     author="Joris Louwen",
     author_email="jorislouwen@hotmail.com",
     description="Detection of biosynthetic sub-clusters",
@@ -33,4 +37,5 @@ setuptools.setup(
           'statsmodels',
           'sympy'
       ],
+    scripts=all_scripts
 )
