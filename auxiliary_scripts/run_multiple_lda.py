aafd55ecbca34d5971ccfbb2b48ba3df3c746df9
Joris Louwen <joris.louwen@wur.nl>
2020-02-07 15:31:37 +0100

    alter to fit new presto_top script

--- auxiliary_scripts/run_multiple_lda.py
+++ auxiliary_scripts/run_multiple_lda.py
@@ -8,8 +8,8 @@ to identify modules.
 import subprocess
 
 if __name__ == '__main__':
-    command = 'python3 lda.py -i /mnt/scratch/louwe015/output_antismashdb_crusemann_mibig_biosynt_subset_19-6/antismashdb_crusemann_mibig_filtered_clusterfile.csv -m /mnt/scratch/louwe015/output_antismashdb_crusemann_mibig_biosynt_subset_19-6/antismashdb_crusemann_mibig_filtered_modules.txt -o /mnt/scratch/louwe015/lda_antismashdb_crusemann_mibig_biosynt_subset_{}_{}27-6_test -C {} -c 10 -t {} {}-f 0.95 -n 15 --classes /mnt/scratch/louwe015/2018-07-13_16-23-26_hybrids_global_crusemann_bgcs_globalmode_mix_mibig_tsvs/big-scape_classes.txt --known_subclusters /mnt/scratch/louwe015/subclusters_subclusterblast_domains_synt_subset.txt --min_genes 2 -I 10'
-    topic_range= [350,500,600,750,1000]
+    command = 'python3 ~/thesis/scripts-thesis/presto_top/presto_top.py -i ipresto_output_build_models_all_sponge_gbks_biosynt_15-1/all_sponge_gbks_clusterfile_filtered_clusterfile.csv -o ipresto_output_build_models_all_sponge_gbks_biosynt_15-1/ipresto_out_100t_1000chnk -t 100 -C 1000 -I 2000 -c 5 --classes ipresto_output_all_sponge_gbks_10-1/all_classes.txt --known_subclusters /mnt/scratch/louwe015/subcluster_data/subclusterblast_data/subclusters_subclusterblast_domains_synt_subset.txt | tee log_ipresto_output_build_models_all_sponge_gbks_biosynt_15-1_ipresto_out_100t_1000chnk.txt'
+    topic_range= [50,75,100,150,200]
     for i in topic_range:
         #without -a
         command_without_a = command.format(i,'','6000',i,'')
