4bb37b6c5dfef57d865c0ca95124ebe5ed72c68c
louwe015 <joris.louwen@wur.nl>
2022-03-14 16:00:08 +0100

    Try out different parameters

--- ipresto/auxiliary_scripts/run_multiple_lda.py
+++ ipresto/auxiliary_scripts/run_multiple_lda.py
@@ -2,28 +2,73 @@
 """
 Author: Joris Louwen
 Script to run LDA multiple times to find good parameters for applying LDA
-to identify modules.
+to identify sub-clusters.
 """
 
 import subprocess
+from sys import argv
+import os
+import time
 
 if __name__ == '__main__':
-    command = 'python3 ~/thesis/scripts-thesis/presto_top/presto_top.py -i ipresto_output_build_models_all_sponge_gbks_biosynt_15-1/all_sponge_gbks_clusterfile_filtered_clusterfile.csv -o ipresto_output_build_models_all_sponge_gbks_biosynt_15-1/ipresto_out_100t_1000chnk -t 100 -C 1000 -I 2000 -c 5 --classes ipresto_output_all_sponge_gbks_10-1/all_classes.txt --known_subclusters /mnt/scratch/louwe015/subcluster_data/subclusterblast_data/subclusters_subclusterblast_domains_synt_subset.txt | tee log_ipresto_output_build_models_all_sponge_gbks_biosynt_15-1_ipresto_out_100t_1000chnk.txt'
-    topic_range= [50,75,100,150,200]
-    for i in topic_range:
-        #without -a
-        command_without_a = command.format(i,'','6000',i,'')
-        print(command_without_a)
-        try:
-            subprocess.check_call(command_without_a, shell=True)
-        except subprocess.CalledProcessError:
-            print(command_without_a)
-            print('all empty topics?')
-        
-        command_with_a = command.format(i,'x10_','60000',i,'-a 10 ')
-        print(command_with_a)
-        try:
-            subprocess.check_call(command_with_a, shell=True)
-        except subprocess.CalledProcessError:
-            print(command_with_a)
-            print('all empty topics?')
+    print("Start - output will be written to", os.getcwd())
+    start = time.time()
+    if len(argv) < 2:
+        raise ValueError("Usage: python run_multiple_lda.py <path_to_ipresto>")
+    ipresto_path = argv[1]
+
+    # see if ipresto is found correctly
+    test_command = f"python {ipresto_path} -h"
+    try:
+        subprocess.check_call(test_command, shell=True)
+    except subprocess.CalledProcessError as e:
+        raise FileNotFoundError("Failed to find <ipresto_path>:", e)
+
+    # hardcoding antismashdb files
+    base = "/mnt/scratch/louwe015/subcluster_data"
+    clust_file = os.path.join(
+        base, "antismashdb_crusemann_mibig_filtered_clusterfile.csv")
+    hmm = os.path.join(base, 'domains', 'Pfam_100subs_tc.hmm')
+    chunksize = '3000'
+    cores = '10'
+    iters = '500'
+    classes = os.path.join(base, 'all_classes.txt')
+    known_subcl = '/mnt/scratch/louwe015/subcluster_data/subclusterblast' \
+                  '_data/subclusters_subclusterblast_domains_synt_subset.txt'
+    biosynt_domains = os.path.join(os.path.split(ipresto_path)[0],
+                                   'files', 'biosynthetic_domains.txt')
+    stat_subcl = '/mnt/scratch/louwe015/iPRESTO/test_ipresto_params/' \
+                 'dummy_presto_stat_subclusters.txt'
+    logfile = 'log_ipresto_output_different_lda_models.txt'
+    command = (
+        'python {} -i bla -o {} --start_from_clusterfile {} -t {} -C {} -I {} '
+        '-c {} --classes {} --known_subclusters {} --include_list {} '
+        '--min_genes 2 --no_redundancy_filtering --visualise '
+        '--stat_subclusters {} --hmm_path {} | tee {}')
+    topic_range = [500, 1000, 1500]
+    alpha_range = ['symmetric']
+    beta_range = [None]  # also called eta
+    for topics in topic_range:
+        for alpha in alpha_range:
+            for beta in beta_range:
+                output_dir = (
+                    f'ipresto_out_topics-{topics}_alpha-{alpha}_beta-{beta}')
+                if not os.path.isdir(output_dir):
+                    formatted_command = command.format(
+                        ipresto_path, output_dir, clust_file, topics,
+                        chunksize, iters, cores, classes, known_subcl,
+                        biosynt_domains, stat_subcl, hmm, logfile)
+
+                    # run the command
+                    print(f"\n###Running, {formatted_command}\n")
+                    try:
+                        subprocess.check_call(formatted_command, shell=True)
+                    except subprocess.CalledProcessError as com_err:
+                        print(
+                            f"Command '{formatted_command}' failed: {com_err}")
+
+                else:
+                    print(f'\n###{output_dir} already exists, not run again\n')
+
+    end = time.time()
+    print(f"\nCompleted in {end-start:.1f}s")
